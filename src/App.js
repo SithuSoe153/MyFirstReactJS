e9a58045e2cf3b31ecd28a637a3d7f0cc96d0b3b
soe <sithu032001@gmail.com>
2023-12-26 15:57:32 +0630

    'Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 
 import { useState } from "react";
+import Navbar from "./components/Navbar/index";
+import PostsList from "./components/PostsList/index";
 
 function App() {
-  // let name = "Sithu Soe";
-  // Use State
-  let [name, setName] = useState("Sithu Soe"); //useState gives getter and setter
   let [posts, setPosts] = useState([
     {
       id: 1,
@@ -24,34 +23,12 @@ function App() {
     },
   ]);
 
-  let changeName = () => {
-    // name = "Kyaw Kyaw";
-    setName("Kyaw Kyaw");
-  };
-
-  let deletePost = (id) => {
-    // console.log(id);
-    setPosts((prevState) => prevState.filter((post) => post.id !== id));
-  };
-
   return (
-    <div className="App">
-      <h1>Hello, {name}</h1>
+    <>
+      <Navbar />
 
-      <button onClick={changeName}>Change Name</button>
-
-      <h1>Posts</h1>
-      <ul>
-        {!!posts.length &&
-          posts.map((posts) => (
-            <li key={posts.id}>
-              {posts.title}
-              <button onClick={() => deletePost(posts.id)}>Delete</button>
-            </li>
-          ))}
-        {!posts.length && <li>No posts yet</li>}
-      </ul>
-    </div>
+      <PostsList posts={posts} /> 
+    </>
   );
 }
 
